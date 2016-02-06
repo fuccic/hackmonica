9287b2cb5a13ae037c5e6f04b46123d51f4b0fe1
ericseandevlin <ericdevlin@me.com>
2016-02-06 14:39:45 -0500

    working on model of shake

---
+++ hackmonica/js/main.js
@@ -0,0 +1,48 @@
+console.log("hieeeeee");
+
+
+var myShakeEvent = new Shake({
+    threshold: 15, // optional shake strength threshold
+    timeout: 1000 // optional, determines the frequency of event generation
+});
+
+//stop listening
+function stopShake(){
+    shakeEvent.stop();
+}
+
+myShakeEvent.start();
+
+window.addEventListener('shake', shakeEventDidOccur, false);
+
+//function to call when shake occurs
+function shakeEventDidOccur () {
+  var element = document.getElementById("box");
+  var r = getRandomInt(0, 255);
+  var g = getRandomInt(0, 255);
+  var b = getRandomInt(0, 255);
+    element.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+    //put your own code here etc.
+    alert('shake!');
+    stopShake();
+}
+
+// jquery debounce or throttle
+
+// var body = $('body');
+//
+// $(window)
+//     .resize( $.debounce( 250, true, function(e){
+//         body.addClass( 'resizing' );
+//     }))
+//     .resize( $.debounce( 250, false, function(e){
+//         body.removeClass( 'resizing' );
+//     }));
+//
+// $(document)
+//     .mousemove( $.debounce( 250, true, function(e){
+//         body.addClass( 'moving' );
+//     }))
+//     .mousemove( $.debounce( 250, false, function(e){
+//         body.removeClass( 'moving' );
+//     }));
