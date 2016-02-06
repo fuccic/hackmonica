8958dd6cb607d93b7716f2f08246bb7c12cd9a16
ericseandevlin <ericdevlin@me.com>
2016-02-06 15:53:40 -0500

    shaking practice

--- public/js/main.js
+++ public/js/main.js
@@ -10,7 +10,7 @@ var element = $("#box");
 
 var myShakeEvent = new Shake({
     threshold: 15, // optional shake strength threshold
-    timeout: 1000 // optional, determines the frequency of event generation
+    timeout: 1 // optional, determines the frequency of event generation
 });
 
 myShakeEvent.start();
